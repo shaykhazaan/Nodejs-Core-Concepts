213bef1896220507f3fe876a35ed4ca2744f525f
shaykhazaan <shaikhaadil00@gmail.com>
2025-02-21 23:56:25 +0530

    node/types added for intellisense | open fs method

--- file_system/app.js
+++ file_system/app.js
@@ -1,12 +1,23 @@
 const fs = require("fs/promises");
+// open
+// read of write
 
 (async () => {
+  const commandFileHandler = await fs.open("file_system/command.txt", "r");
+
   const watcher = fs.watch("file_system/command.txt"); //watch method can watch files or whole directory
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
       // the file has changed
       console.log("The file has changed.");
+
+      // get size of file
+      const size = (await commandFileHandler.stat()).size;
+
+      // read content of the file
+      const content = await commandFileHandler.read();
+      console.log("commandFileHandler CONTENT::: \n", content);
     }
   }
 })();
